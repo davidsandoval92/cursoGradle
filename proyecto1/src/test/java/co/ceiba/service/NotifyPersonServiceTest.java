65a6d998b1ce2ddce4344e916ba5a088ded3113c
David Sandoval <davidsandoval9217@gmail.com>
2018-01-16 11:47:40 -0500

    Primer commit con git bash

---
+++ proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -0,0 +1,34 @@
+package co.ceiba.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import co.ceiba.domain.Person;
+import co.ceiba.testdatabuilder.PersonTestDataBuilder;
+
+public class NotifyPersonServiceTest {
+
+	private NotifyPersonService notifyPersonService;
+	private EmailService emailService;
+
+	@Before
+	public void setup() {
+		emailService = Mockito.mock(EmailService.class);//new EmailService();
+		notifyPersonService = new NotifyPersonService(emailService);
+	}
+
+	@Test
+	public void notifyTest() {
+		// Arrange
+		Mockito.when(emailService.sendEmail(Mockito.anyString())).thenReturn("hola mundo");
+		Person person = new PersonTestDataBuilder().buil();
+		// Act
+		String message = notifyPersonService.notify(person);
+		System.out.println(message);
+		// Assert
+		Assert.assertNotNull(message);
+	}
+
+}
